4cd36f614688a6985b7a53edbe36f80c2988155c
Nguyễn Tấn Dũng <91122031+dungnguyen73@users.noreply.github.com>
2023-08-12 22:20:35 +0700

    Add files via upload
    push public folder 
    make search bar in the nav work

---
+++ public/nav.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Nike. Just Do It</title>
+    <style>
+      body{
+        font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+      }
+      
+    </style>
+</head>
+
+<!-- navigator bar section -->
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+        <a href="main.php" class="navbar-brand"><img src="assets/Logo_NIKE.svg.png" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsebtn" aria-controls="collapsebtn" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="collapsebtn"> <!-- Removed unnecessary classes -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="shoes.php" class="nav-link">Shoes</a>
+                </li>
+                <li class="nav-item">
+                    <a href="clothing.php" class="nav-link">Clothing</a>
+                </li>
+                <li class="nav-item">
+                    <a href="accessory.php" class="nav-link">Accessories</a>
+                </li>
+            </ul>
+            <div class="d-md-none options"> <!-- Corrected class name -->
+                <i class="fas fa-heart fa-lg" title="Favorites"></i> <!-- Corrected class name and icon prefix -->
+                <i class="fas fa-shopping-bag fa-lg" title="Bag items"></i> <!-- Corrected class name and icon prefix -->
+                <i class="fas fa-user fa-lg" title="User"></i> <!-- Corrected class name and icon prefix -->
+            </div>
+        </div>
+
+        <div class="d-none d-md-block" id="searchbar">
+            <form class="form-inline" method="get" action = "searchresult.php" >
+                <input class="form-control mr-md-2" type="search" placeholder="Search" aria-label="Search" name='search_term' >
+            </form>
+        </div>
+
+        <div class="d-none d-md-block options">
+            <i class="far fa-heart fa-lg" title="Favorites"></i> <!-- Corrected class name and icon prefix -->
+            <i class="fas fa-shopping-bag fa-lg" title="Bag items"></i>
+            <i class="far fa-user fa-lg" title="User"></i>
+        </div>
+    </div>
+</nav>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    
---
+++ public/searchresult.php
@@ -0,0 +1,66 @@
+<?php
+include 'nav.php';
+include '../mvc/model/db_config.php';
+
+if (isset($_GET['search_term'])) {
+    // User has submitted a search
+    $searchTerm = $_GET['search_term'];
+    
+    // Perform your search logic here using the $searchTerm
+    // For example, query your database for matching products and display results
+    
+    $db = new Database();
+    $db->connect();
+    $search_result = $db->searchAcrossAllTables($searchTerm);
+    $totalProduct = 0;
+    
+    if (!empty($search_result)) {
+        foreach ($search_result as $tablename => $results) {
+            $totalProduct += count($results);
+            // ... your existing code to display search results ...
+        }
+?>
+<div class="container-fluid my-2">
+    <div class="title my-4">
+        Search result for <br>
+            <h4><?php echo $searchTerm . " (" . $totalProduct . ")"; ?></h4>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-12">
+        </div>
+        <div class="col-lg-9 px-0">
+            <div class="row">
+        <?php
+        foreach ($search_result as $tablename => $results) {
+            foreach ($results as $value) {
+                ?>
+                <div class="col-12 col-sm-6 col-lg-4 px-1">
+                    <div class="card border-0">
+                        <img src="<?php echo $value['image_path']; ?>" alt="" loading="lazy">
+                        <div class="card-body mx-0 my-2 py-0">
+                            <p class="card-text note my-0"><?php echo $value['product_note']; ?></p>
+                            <a href="#" class="text-dark no-underline">
+                                <h5 class="card-title product-name"><?php echo $value['product_name']; ?></h5>
+                            </a>
+                            <p class="card-text text-secondary clothing-type"><?php echo $value['product_subcategory']; ?></p>
+                            <p class="card-text price mt-2"><?php echo $value['product_price'] . " USD"; ?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+        }
+        ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    } else {
+        echo "Cannot find suitable products";
+    }
+} else {
+    // No search term submitted
+    echo "No search term submitted.";
+}
+?>
