f3e7773d1d10e09898d4fbbd6a7603a795e8c6ad
Nguyễn Tấn Dũng <91122031+dungnguyen73@users.noreply.github.com>
2023-08-15 20:04:07 +0700

    Add files via upload
    add some animations to the main homepage

--- public/footer.php
+++ public/footer.php
@@ -171,12 +171,8 @@
 
     <!-- end footer section -->
 
-    <!-- scripts -->
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
     <script src="https://kit.fontawesome.com/a1037a3a28.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@react-spring/parallax@9.7.3/dist/cjs/index.min.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script>
       let items = document.querySelectorAll('.carousel .carousel-item')
@@ -194,6 +190,34 @@
               next = next.nextElementSibling
           }
       })
+
+      let attractiveImg= document.getElementById('attraction-img');
+const ImglistPaths = ['assets/homepage/pic3.jpg',
+                    'assets/homepage/pic4.jpg',
+                    'assets/homepage/pic6.jpg'
+                    ]
+let i =0;
+setInterval(changeAttractImg, 5000);
+function changeAttractImg(){
+    attractiveImg.src= ImglistPaths[(i++)%ImglistPaths.length];
+}
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show")
+        }
+    });
+});
+
+hiddenElements.forEach((el)=> observer.observe(el) );
 
     </script>
     <script src="script.js"></script>
--- public/script.js
+++ public/script.js
@@ -29,3 +29,18 @@ let items = document.querySelectorAll('.carousel .carousel-item')
       })
 
 //
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show")
+        }
+    });
+});
+
+hiddenElements.forEach((el)=> observer.observe(el) );
